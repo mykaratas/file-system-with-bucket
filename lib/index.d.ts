4d71df2102b4ab0efb34bb9e71032256728a69ac
Malik Karatas <malik.karatas@atez.com.tr>
2021-03-07 17:27:32 +0300

    Implemented ftp, fs, minio, ssh2 sftp connector from File System Config.

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,1 +1,85 @@
+import { Client as MinioClient, ClientOptions } from 'minio';
 export declare const Greeter: (name: string) => string;
+export interface Factory {
+    client?: MinioClient;
+    put(): any;
+    get(): any;
+    list(): any;
+}
+export declare class MinioFactory implements Factory {
+    put(): void;
+    get(): void;
+    list(): void;
+}
+export declare class LocaleFactory implements Factory {
+    put(): void;
+    get(): void;
+    list(): void;
+}
+export declare class FtpFactory implements Factory {
+    put(): void;
+    get(): void;
+    list(): void;
+}
+export declare class SftpFactory implements Factory {
+    put(): void;
+    get(): void;
+    list(): void;
+}
+export interface Put {
+    put(): any;
+}
+export interface Get {
+    get(): any;
+}
+export interface List {
+    list(): any;
+}
+export declare class MinioPut implements Put {
+    put(): void;
+}
+export declare class FtpPut implements Put {
+    put(): void;
+}
+export declare class SftpPut implements Put {
+    put(): void;
+}
+export declare class LocalePut implements Put {
+    put(): void;
+}
+export declare class MinioList implements List {
+    list(): void;
+}
+export declare class FtpList implements List {
+    list(): void;
+}
+export declare class SftpList implements List {
+    list(): void;
+}
+export declare class LocaleList implements List {
+    list(): void;
+}
+export declare class MinioGet implements Get {
+    get(): void;
+}
+export declare class FtpGet implements Get {
+    get(): void;
+}
+export declare class SftpGet implements Get {
+    get(): void;
+}
+export declare class LocaleGet implements Get {
+    get(): void;
+}
+export declare class FileSystem implements Factory {
+    private factory;
+    constructor(factory: Factory);
+    put(): any;
+    get(): any;
+    list(): any;
+}
+export interface Config extends ClientOptions {
+}
+export declare class FileSystemConfig {
+    constructor(client: MinioClient);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,201 @@
+import { Client as MinioClient, ClientOptions as MinioClietOptions } from 'minio';
+import SftpClient, { ConnectOptions } from 'ssh2-sftp-client';
+import { AccessOptions, Client as FtpClient } from 'basic-ftp';
 export const Greeter = (name: string) => `Hello ${name}`;
+
+export interface Factory {
+  client?: MinioClient | SftpClient | FtpClient;
+  put(): any;
+  get(): any;
+  list(): any;
+}
+
+export class MinioFactory implements Factory {
+  client: MinioClient;
+  constructor(minioClientOptions: MinioClietOptions) {
+    this.client = new MinioClient(minioClientOptions);
+  }
+  put() {
+    throw new Error('Method not implemented.');
+  }
+  get() {
+    throw new Error('Method not implemented.');
+  }
+  list() {
+    throw new Error('Method not implemented.');
+  }
+}
+
+export class FtpFactory implements Factory {
+  client: FtpClient;
+  constructor(ftpClientOptions: FtpClientOptions) {
+    this.client = new FtpClient(ftpClientOptions.timeout);
+  }
+  put() {
+    throw new Error('Method not implemented.');
+  }
+  get() {
+    throw new Error('Method not implemented.');
+  }
+  list() {
+    throw new Error('Method not implemented.');
+  }
+}
+export class LocaleFactory implements Factory {
+  put() {
+    throw new Error('Method not implemented.');
+  }
+  get() {
+    throw new Error('Method not implemented.');
+  }
+  list() {
+    throw new Error('Method not implemented.');
+  }
+}
+export class SftpFactory implements Factory {
+  client: SftpClient;
+  constructor(sftpClientOptions: SftpClientOptions) {
+    this.client = new SftpClient();
+    this.client.connect(sftpClientOptions);
+  }
+
+  put() {
+    throw new Error('Method not implemented.');
+  }
+  get() {
+    throw new Error('Method not implemented.');
+  }
+  list() {
+    throw new Error('Method not implemented.');
+  }
+}
+export interface Put {
+  put(): any;
+}
+export interface Get {
+  get(): any;
+}
+export interface List {
+  list(): any;
+}
+
+export class MinioPut implements Put {
+  put() {
+    throw new Error('Method not implemented.');
+  }
+}
+
+export class FtpPut implements Put {
+  put() {
+    throw new Error('Method not implemented.');
+  }
+}
+
+export class SftpPut implements Put {
+  put() {
+    throw new Error('Method not implemented.');
+  }
+}
+
+export class LocalePut implements Put {
+  put() {
+    throw new Error('Method not implemented.');
+  }
+}
+
+export class MinioList implements List {
+  list() {
+    throw new Error('Method not implemented.');
+  }
+}
+
+export class FtpList implements List {
+  list() {
+    throw new Error('Method not implemented.');
+  }
+}
+
+export class SftpList implements List {
+  list() {
+    throw new Error('Method not implemented.');
+  }
+}
+
+export class LocaleList implements List {
+  list() {
+    throw new Error('Method not implemented.');
+  }
+}
+
+export class MinioGet implements Get {
+  get() {
+    throw new Error('Method not implemented.');
+  }
+}
+
+export class FtpGet implements Get {
+  get() {
+    throw new Error('Method not implemented.');
+  }
+}
+
+export class SftpGet implements Get {
+  get() {
+    throw new Error('Method not implemented.');
+  }
+}
+
+export class LocaleGet implements Get {
+  get() {
+    throw new Error('Method not implemented.');
+  }
+}
+
+export class FileSystem implements Factory {
+  constructor(private factory: Factory) {}
+  put() {
+    return this.factory.get();
+  }
+  get() {
+    return this.factory.get();
+  }
+  list() {
+    return this.factory.list();
+  }
+}
+
+export interface MinioClientOptions extends MinioClietOptions {}
+
+export interface SftpClientOptions extends ConnectOptions {
+  host: string;
+  username: string;
+  password: string;
+  port?: number;
+}
+
+export interface FtpClientOptions extends AccessOptions {
+  host?: string;
+  port?: number;
+  user?: string;
+  password?: string;
+  timeout?: number;
+}
+
+export class FileSystemConfig {
+  private factory: Factory;
+  constructor(
+    clientOptions: SftpClientOptions | MinioClietOptions | FtpClientOptions,
+    clientType: 'minio' | 'aws' | 'sftp' | 'ftp' | 'locale' = 'locale',
+  ) {
+    if (clientType === 'sftp') {
+      this.factory = new SftpFactory(clientOptions as SftpClientOptions);
+    } else if (clientType === 'minio') {
+      this.factory = new MinioFactory(clientOptions as MinioClientOptions);
+    } else if (clientType === 'ftp') {
+      this.factory = new FtpFactory(clientOptions as FtpClientOptions);
+    } else {
+      this.factory = new LocaleFactory();
+    }
+    new FileSystem(this.factory);
+  }
+}
